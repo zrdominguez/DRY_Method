cb74dfadc0cd2e11957d8e77ac6d09936010790a
zrdominguez <zechariahd@gmail.com>
2024-03-05 19:17:04 -0600

    Used DRY method to simplify functions

--- practice.js
+++ practice.js
@@ -1,45 +1,53 @@
+function biggerThan(param1, param2){
+  return param1 > param2;
+}
+
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  // let bigNum;
+  // if (num1 > num2) {
+  //   bigNum = num1;
+  //   return bigNum * 2;
+  // } else {
+  //   bigNum = num2;
+  //   return bigNum * 2;
+  // }
+  return biggerThan(num1, num2) ? num1 * 2 : num2 * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  // let bigNum;
+  // if (num1 > num2) {
+  //   bigNum = num1;
+  //   return bigNum / 3;
+  // } else {
+  //   bigNum = num2;
+  //   return bigNum / 3;
+  // }
+  return biggerThan(num1, num2) ? num1 / 3 : num2 / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  // let bigNum;
+  // if (sum1 > sum2) {
+  //   bigNum = sum1;
+  //   return `I ate ${bigNum} tacos.`;
+  // } else {
+  //   bigNum = sum2;
+  //   return `I ate ${bigNum} tacos.`;
+  // }
+  return `I ate ${biggerThan(sum1, sum2) ? sum1 : sum2} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  // let smallDog;
+  // if (weight1 < weight2) {
+  //   smallDog = weight1;
+  //   return `I adopted a dog that weighs ${smallDog} pounds.`;
+  // } else {
+  //   smallDog = weight2;
+  //   return `I adopted a dog that weighs ${smallDog} pounds.`;
+  // }
+  return `I adopted a dog that weighs ${biggerThan(weight1, weight2) ? weight2 : weight1} pounds.`
 }
 
 
